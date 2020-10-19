7418a9e94ddca22badd3095b5c10d77d1db77a5c
John Garcia <johngarciadev@gmail.com>
2020-10-19 10:48:08 -0700

    added global variables and function stubs to app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startButton = document.querySelector('.btn__reset');
+const missed = 0;
+const phrases = ["Why so serious", "Hasta la vista baby", "May the force be with you", "Hulk smash", "Just keep swimming"];
+
+
+// return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+
+}
+
+// adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+}
+
+// check if a letter is in the phrase
+const checkLetter = button => {
+
+}
+
+// check if the game has been won or lost
+const checkWin = () => {
+
+}
+
+// listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+
+});
+
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+    
+    
+});
